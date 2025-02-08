b71641f91c2026c91865f10ac9fdfa40654da461
Rahul Srivastava <130776919+RahulSrvst@users.noreply.github.com>
2025-02-08 12:16:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,22 +2,45 @@ require("./Models/db.js");
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const { Server } = require("socket.io");
 const bodyParser = require("body-parser");
+const AuthRouter = require("./Routes/AuthRouter.js");
 const serverless = require("serverless-http");
 
-dotenv.config();
+dotenv.config(); // Initialize environment variables
+
+const port = process.env.PORT || 8020;
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors());
-
+app.use(cors()); 
 app.get("/", (req, res) => {
   res.send("Welcome to Smart Campus!");
 });
-
-const AuthRouter = require("./Routes/AuthRouter.js");
 app.use("/auth", AuthRouter);
 
-module.exports = app;
+const httpServer = app.listen(port, () => {
+  console.log(`Server running successfully at PORT ${port}!`);
+});
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected:", socket.id);
+  });
+});
+
 module.exports.handler = serverless(app);
+module.exports = app;
